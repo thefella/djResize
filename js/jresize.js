9b8fe178b75c56e378e4022b23def36b06327a5f
toddmotto <create@toddmotto.com>
2012-10-29 16:15:10 +0000

    Update js/jresize.js

--- js/jresize.js
+++ js/jresize.js
@@ -10,18 +10,16 @@
     $.jResize = function () {
 
         // Define variables
-        var resizer = '<div class="viewports" style="position:fixed;top:0;left:0;right:0;z-index:9999;min-height:38px;display:none;background:#444;color:#FFF;border-bottom:2px solid #FFF;"><ul class="viewlist"></ul><div style="clear:both;"></div></div>';
+        var resizer = '<div class="viewports" style="visibility:visible;position:fixed;top:0;left:0;right:0;overflow:auto;z-index:9999;background:#444;color:#FFF;box-shadow:0 0 3px #222;"><ul class="viewlist"></ul><div style="clear:both;"></div></div>';
         var viewPortWidths = ["320px", "480px", "540px", "600px", "768px", "960px", "1024px", "1280px"];
         var viewPortList = 'display:inline-block;cursor:pointer;font-size:12px;line-height:12px;text-align:center;width:6%;border-right:1px solid #555;padding:13px 5px;'
-        var credit = '<div style="float:right;padding:13px 25px;font-size:12px;line-height:12px;">jResize Plugin by <a href="http://toddmotto.com" style="color:#FFF;text-decoration:underline;">Todd Motto</a></div>'
+        var credit = '<div style="float:right;padding:13px 25px;font-size:12px;line-height:12px;">jResize Plugin by <a href="http://toddmotto.com" style="color:#FFF;text-decoration:underline;">Todd Motto</a></div>';
 
         // Wrap all HTML inside the <body>
         $('body').wrapInner('<div id="resizer" />');
 
         // Insert our resizing plugin
-        $('#resizer').css({
-            margin: '0 auto'
-        }).before(resizer);
+        $('#resizer').before(resizer);
 
         // Loop through the array, using the each to dynamically generate our ViewPort lists
         $.each(viewPortWidths, function (go, className) {
@@ -35,6 +33,11 @@
 
         // Prepend our Reset button
         $('.viewlist').prepend('<li class="reset" style="' + viewPortList + '">Reset</li>', credit);
+        
+        // Slidedown the viewport navigation and animate the resizer
+        var height = $('.viewlist').outerHeight();
+        $('.viewports').hide().slideDown('300');
+        $('#resizer').animate({marginTop : height});
 
         // Allow for Reset
         $('.reset').click(function () {
@@ -42,13 +45,6 @@
                 width: 'auto'
             });
         });
-
-        // Slidedown the viewport navigation and animate the resizer
-        $('.viewports').slideDown('300');
-        $('#resizer').animate({
-            marginTop: '40px'
-        });
-
     };
 
 })(jQuery);
